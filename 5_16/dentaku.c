a33801630b16919966ca405cc9ea406dc667c8b5
yuria419 <tjg-690-grm@e-net.nara.jp>
2025-05-19 06:56:08 +0000

    5_19

--- 5_16/dentaku.c
+++ 5_16/dentaku.c
@@ -14,36 +14,42 @@ int main(void){
 
     if(mode==1){
         printf("足し算を選択しました。\n");
-    }else if(){
+        n=tasu(a,b);
+    }else if(mode==2){
         printf("引き算を選択しました。\n");
-    }else if(){
+        n=hiku(a,b);
+    }else if(mode==3){
         printf("掛け算を選択しました。\n");
+        n=kakeru(a,b);
     }else{
         printf("不正なモードです。\n");
     }
+    printf("答えは%fです。\n",n);
     
     return 0;
 }
 
-switch(mode){
-    case 1:
-        printf("足し算を選択しました。\n");
-        n=tasu(a,b);
-        break;
-    case 2:
-        printf("引き算を選択しました。\n");
-        n=hiku(a,b);
-        break;
-    case 3:
-        printf("掛け算を選択しました。\n");
-        n=kakeru(a,b);
-        break;
-    default:
-        printf("不正なモードです。\n");
-    }
-    printf("答えは%fです。\n",n);
-    return 0;
-}
+// switch(mode){
+//     case 1:
+//         printf("足し算を選択しました。\n");
+//         n=tasu(a,b);
+//         break;
+//     case 2:
+//         printf("引き算を選択しました。\n");
+//         n=hiku(a,b);
+//         break;
+//     case 3:
+//         printf("掛け算を選択しました。\n");
+//         n=kakeru(a,b);
+//         break;
+//     default:
+//         printf("不正なモードです。\n");
+//     }
+//     printf("答えは%fです。\n",n);
+//     return 0;
+// }
+
+
 float tasu(float a,float b){
     return a+b;
 }
